b38c02da5cba82b500c99452e027c2207f94e21c
0xClandestine <clandestine.eth@proton.me>
2023-02-17 23:12:34 -0600

    📜 test -> example

--- sign-message-hash.py
+++ sign-message-hash.py
@@ -1,9 +1,10 @@
 from secp256k1 import Signer
 
-# test private key
+# example private key
 private_key = 0xF9457396F73E9DFBCC00205FB0345196F33449648B200D709F9A62F4B5329F58
 
-# test eip712 message hash
+# example message hash
 message_hash = 0x0050A397BBAAE4C5BD1C2CB2FDB76BAAA14AE2EF0BADA7723ECC2BE9055FCBE9
 
+# example output, given private key and message hash
 print(Signer(private_key).sign_hash(message_hash, print_qr=False))
